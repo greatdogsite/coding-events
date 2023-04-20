2ed71a2580ee1156db417b2e77e5724b878daa2e
Mango <greatdogsite@gmail.com>
2023-04-20 18:06:37 -0500

    saving

--- src/main/java/com/mango/codingevents/controllers/EventController.java
+++ src/main/java/com/mango/codingevents/controllers/EventController.java
@@ -4,16 +4,32 @@ package com.mango.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    @RequestMapping()
-    public String event(Model model){
-        ArrayList<String> names = new ArrayList<>(Arrays.asList("denny","vlad","alexey"));
+    private static List<String> names = new ArrayList<>();
+
+    //@GetMapping("index")
+    @GetMapping()
+    public String displayAllEvents(Model model){
         model.addAttribute("names",names);
         return "events/index";
+    }
+
+    // lives at events/create
+    @RequestMapping("create") //default is GET
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName){
+        names.add(eventName); //add to private static arraylist
+//        return "redirect:index"; //redirect is 300 level http redirect to events/index
+        return "redirect:";
     }
 }
